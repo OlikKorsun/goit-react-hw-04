5012024b70a72b5bbc3dbbe6c2a850ca68a306b5
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-04-19 01:01:33 +0300

    create main structure + import axios

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './App.module.css'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+  return (
+      <h1>GoIT-React-HW-04</h1>
+  )
+}
